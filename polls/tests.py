61f68bad53aea1653ae1f64dc126aa7bfaf1513f
VictorBoYang <VictorBoYang@gmail.com>
2019-11-10 17:15:15 -0500

    finish staticfile learning

--- polls/tests.py
+++ polls/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
+from django.utils import timezone
+from .models import Question
+import datetime
 
-# Create your tests here.
+
+class Question_model_tests(TestCase):
+    def test_was_published_recently_with_future_question(self):
+        time = timezone.now() + datetime.timedelta(days=30)
+        future_question = Question(pub_date=time)
+        self.assertIs(future_question.was_published_recently(), False)
+
+# learn test later,not finish
